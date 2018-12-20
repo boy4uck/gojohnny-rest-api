069e00ba0f110a2d2c526692b7714db8a61cd807
yura-jt <cryobyte.zero@gmail.com>
2018-12-20 07:38:16 +0200

    37 deleted changes, made by previous commit from feature/37    add H2 geo spatial dialect;    change spring boot version to 1.5.10;    set show-sql property true for profile "testing";    fix flyway property for using with spring boot 1.x

--- src/main/java/co/gojohnny/model/Place.java
+++ src/main/java/co/gojohnny/model/Place.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.math.BigDecimal;
 
-import static javax.persistence.GenerationType.SEQUENCE;
+import static javax.persistence.GenerationType.*;
 
 @NoArgsConstructor
 @Getter
@@ -29,7 +29,7 @@ public class Place {
     @Column
     private BigDecimal altitude;
 
-    @Column(nullable = false, columnDefinition = "geometry")
+    @Column(nullable = false)
     private Point location;
 
     @Column(name = "photo_url")
