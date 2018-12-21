8c256e3d17226a1d3aa1b818847db3170471acac
shtramak <shtramak@gmail.com>
2018-12-21 15:19:31 +0200

    add skeleton for PhotoComment without mapping on Photo

---
+++ src/main/java/co/gojohnny/model/PhotoComment.java
@@ -0,0 +1,37 @@
+package co.gojohnny.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "photo_comment")
+public class PhotoComment {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String text;
+
+    @Column(name = "creation_time", nullable = false)
+    private LocalDateTime creationTime = LocalDateTime.now();
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // private Photo photo;
+}
